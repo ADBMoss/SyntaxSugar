2ef90d4f4dcc6148d4994328ab136577c8586daa
DarriusB <darrius.bradford@gmail.com>
2022-04-26 08:38:57 -0700

    Syntax Sugar

---
+++ SyntaxSugar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SyntaxSugar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sugar());
+        }
+
+        public static string Sugar ()
+        {
+            var answer = 4;
+            var answer2 = "is less than nine";
+            var answer3 = " is greater than or equal to nine";
+            var response = (answer < 9) ? $"{answer2}" : $"{answer3}";
+
+            return response;
+        }
+
+
+
+
+    }
+}
