3d768d827c96d276bc3d1d0545a7b421cf5066c1
DarriusB <darrius.bradford@gmail.com>
2022-04-26 09:06:10 -0700

    updated my code to be more readable

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SyntaxSugar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sugar());
+        }
+
+        public static string Sugar ()
+        {
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+
+            return response;
+        }
+    }
+}
